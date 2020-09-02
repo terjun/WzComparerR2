1d512d9cf683496dee388a9f9f346d9665f900f0
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2020-09-02 11:16:13 +0900

    [KMS] CharaSim: Fix escape character in set item tooltip

--- WzComparerR2/CharaSimControl/GearGraphics.cs
+++ WzComparerR2/CharaSimControl/GearGraphics.cs
@@ -236,7 +236,7 @@ namespace WzComparerR2.CharaSimControl
         /// <param Name="x">起始的x坐标。</param>
         /// <param Name="X1">每行终止的x坐标。</param>
         /// <param Name="y">起始行的y坐标。</param>
-        public static void DrawString(Graphics g, string s, Font font, int x, int x1, ref int y, int height, Color? orangeColor = null)
+        public static void DrawString(Graphics g, string s, Font font, int x, int x1, ref int y, int height, Color? orangeColor = null, Color? textColor = null)
         {
             if (s == null)
                 return;
@@ -245,7 +245,7 @@ namespace WzComparerR2.CharaSimControl
             {
                 r.WordWrapEnabled = false;
                 r.UseGDIRenderer = true;
-                r.DrawString(g, s, font, x, x1, ref y, height, orangeColor);
+                r.DrawString(g, s, font, x, x1, ref y, height, orangeColor, textColor);
             }
         }
 
@@ -530,12 +530,12 @@ namespace WzComparerR2.CharaSimControl
             Color defaultColor;
             Color orangeColor;
 
-            public void DrawString(Graphics g, string s, Font font, int x, int x1, ref int y, int height, Color? orangeColor = null)
+            public void DrawString(Graphics g, string s, Font font, int x, int x1, ref int y, int height, Color? orangeColor = null, Color? textColor = null)
             {
                 //初始化环境
                 this.g = g;
                 this.drawX = x;
-                this.defaultColor = Color.White;
+                this.defaultColor = (textColor == null ? Color.White : (Color)textColor);
                 if (orangeColor != null)
                 {
                     this.orangeColor = (Color)orangeColor;
